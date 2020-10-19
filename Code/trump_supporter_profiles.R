0f1bf7fd7769976e5d64a8856e146a09d72ae49a
apoorvabhide <apoorva.bhide95@gmail.com>
2020-10-19 23:08:18 +0530

    Added profiling code

--- Code/trump_supporter_profiles.R
+++ Code/trump_supporter_profiles.R
@@ -5,7 +5,6 @@ library(dplyr)
 library(ggplot2)
 library(cluster)
 library(reshape2)
-library(mclust)
 setwd("~/Documents/Writing/Trump Supporter/")
 # Load the data and choose required columns
 cces_ts <- readRDS("Data/cces_2018_data.RDS")
@@ -13,22 +12,7 @@ cces_ts <- readRDS("Data/cces_2018_data.RDS")
 # saveRDS(cces_ts,"./cces_2018_data.RDS")
 cces <- cces_ts %>% select(caseid=caseid,
                            trumpapprove=CC18_308a,
-                           # birthyr,
-                           # gender,
-                           # sexuality,
-                           # educ,
-                           # race,
-                           # ideo5,
-                           # pew_churatd,
-                           # newsint,
-                           # union,
-                           # investor,
-                           # tvuse=CC18_300_2 ,
-                           # newspaper=CC18_300_3,
-                           # radiouse=CC18_300_4,
                            policespending=CC18_426_4,
-                           # videoabtpolitics=CC18_300d_3,
-                           # fwdpolitics=CC18_300d_5,
                            alwaysallowabortion=CC18_321a,
                            exceptionabortion=CC18_321b,
                            allowemployersdenyabortion=CC18_321d,
@@ -49,9 +33,6 @@ cces <- cces_ts %>% select(caseid=caseid,
                            withdrawparis=CC18_332c,
                            withdrawtpp = CC18_332e,
                            selfideology=CC18_334A,
-                           # trumpideology=CC18_334C,
-                           # demideology=CC18_334D,
-                           # repideology=CC18_334E,
                            russiacollusion=CC18_335,
                            epaco2=CC18_415a,
                            environprot = CC18_415d,
@@ -69,7 +50,6 @@ cces <- cces_ts %>% select(caseid=caseid,
                            welfarespend=CC18_426_1,
                            healthcarespend=CC18_426_2,
                            educationspend = CC18_426_3,
-                           # pew_bornagain,
                            banassault=CC18_320c,
                            backgroundchecks=CC18_320a,
                            easierconcealed=CC18_320d,
@@ -92,9 +72,10 @@ for(i in 1:ncol(cces_cluster))
 
 cces_cluster1 <- cces_cluster
 # creating distance matrix
+# set.seed(72)
 distmatrix <- daisy(x = cces_cluster1,metric = "gower")
-set.seed(423)
 plot(hclust(distmatrix,method = "complete"))
+set.seed(72)
 hc <- hclust(distmatrix,method="complete")
 cces_cluster1$caseid <- caseids
 cces_cluster1$ClusterAssigned <- cutree(hc,k=4)
@@ -125,6 +106,7 @@ cces_profile <- cces_ts %>% select(caseid=caseid,
                            postedabtpolitics = CC18_300d_1,
                            householdincomelastyear=CC18_302,
                            union,
+                           demideology=CC18_334D,
                            investor,
                            tvuse=CC18_300_2 ,
                            newspaper=CC18_300_3,
@@ -152,26 +134,38 @@ cluster_summary <- function(df,clusterNo)
   return(responses)
 }
 
-caseids <- cces_cluster2$caseid
-distmatrix <- daisy(x = cces_cluster2[,-45],metric = "gower")
-set.seed(423)
-plot(hclust(distmatrix,method = "complete"))
-hc <- hclust(distmatrix,method="complete")
-cces_cluster2$caseid <- caseids
-cces_cluster2$ClusterAssigned <- cutree(hc,k=3)
-cces_cluster2 %>% group_by(ClusterAssigned) %>% dplyr::summarise(Responses = length(unique(caseid))) %>% 
-  mutate(ResponsesPC=Responses/sum(Responses))
-
 clust1 <- cluster_summary(cces_cluster2,clusterNo = 1) %>% rename(C1Freq=Freq,C1FreqPC=FreqPC)
 clust2 <- cluster_summary(cces_cluster2,clusterNo = 2) %>% rename(C2Freq=Freq,C2FreqPC=FreqPC)
 clust3 <- cluster_summary(cces_cluster2,clusterNo = 3) %>% rename(C3Freq=Freq,C3FreqPC=FreqPC)
 clust4 <- cluster_summary(cces_cluster2,clusterNo = 4) %>% rename(C4Freq=Freq,C4FreqPC=FreqPC)
 
-clusters <- clust1 %>% full_join(clust2) %>% full_join(clust3)# %>% full_join(clust4)
+clusters <- clust1 %>% full_join(clust2) %>% full_join(clust3) %>% full_join(clust4)
 
 # analysing & visualising the entire base first
-c1 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==1),aes(x=faminc_new)) + geom_histogram(stat="count")
-c2 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==2),aes(x=faminc_new)) + geom_histogram(stat="count")
-c3 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==3),aes(x=faminc_new)) + geom_histogram(stat="count")
-# c4 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==4),aes(x=faminc_new)) + geom_histogram(stat="count")
+c1 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==1),aes(x=birthyr)) + geom_histogram(stat="count")+xlim(1920,2000)
+c2 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==2),aes(x=birthyr)) + geom_histogram(stat="count")+xlim(1920,2000)
+c3 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==3),aes(x=birthyr)) + geom_histogram(stat="count")+xlim(1920,2000)
+c4 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==4),aes(x=birthyr)) + geom_histogram(stat="count")+xlim(1920,2000)
 gridExtra::grid.arrange(c1,c2,c3,c4,ncol=1)
+gridExtra::grid.arrange(c1,c2,c3,ncol=1)
+
+f1 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==1,
+                                      !is.na(faminc_new)),aes(x=faminc_new)) + geom_histogram(stat="count")
+f2 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==2),aes(x=faminc_new)) + geom_histogram(stat="count")
+f3 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==3),aes(x=faminc_new)) + geom_histogram(stat="count")
+f4 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==4),aes(x=faminc_new)) + geom_histogram(stat="count")
+gridExtra::grid.arrange(f1,f2,f3,f4,ncol=1)
+gridExtra::grid.arrange(f1,f2,f3,ncol=1)
+
+d1 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==1,
+                                      !is.na(newsint)),aes(x=newsint)) + geom_histogram(stat="count")
+d2 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==2,
+                                      !is.na(newsint)),aes(x=newsint)) + geom_histogram(stat="count")
+d3 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==3,
+                                      !is.na(newsint)),aes(x=newsint)) + geom_histogram(stat="count")
+d4 <- ggplot(cces_cluster2 %>% filter(ClusterAssigned==4,
+                                      !is.na(newsint)),aes(x=newsint)) + geom_histogram(stat="count")
+gridExtra::grid.arrange(d1,d2,d3,d4,ncol=1)
+
+ide <- cces_cluster2 %>% group_by(ClusterAssigned,selfideology,demideology) %>% dplyr::summarise(Responses=length(unique(caseid)))
+ggplot(ide,aes(x=selfideology,y=demideology,colour=as.factor(ClusterAssigned))) + geom_point()
